81641296a14d177b6b6f94e0ce2933a09e3978a2
Alexsandro-araujo <araujoalexsandro463@gmail.com>
2026-06-09 17:34:23 -0300

    Projeto calculadora WinForms

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.soma = new System.Windows.Forms.Button();
+            this.menos = new System.Windows.Forms.Button();
+            this.multiplicacao = new System.Windows.Forms.Button();
+            this.divisao = new System.Windows.Forms.Button();
+            this.txtVisor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CALCULADORA";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(12, 101);
+            this.txtN1.Multiline = true;
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(80, 55);
+            this.txtN1.TabIndex = 1;
+            this.txtN1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(12, 177);
+            this.txtN2.Multiline = true;
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(80, 51);
+            this.txtN2.TabIndex = 2;
+            this.txtN2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // soma
+            // 
+            this.soma.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.soma.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.soma.Location = new System.Drawing.Point(275, 101);
+            this.soma.Name = "soma";
+            this.soma.Size = new System.Drawing.Size(157, 55);
+            this.soma.TabIndex = 3;
+            this.soma.Text = "+";
+            this.soma.UseVisualStyleBackColor = true;
+            this.soma.Click += new System.EventHandler(this.soma_Click);
+            // 
+            // menos
+            // 
+            this.menos.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menos.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.menos.Location = new System.Drawing.Point(275, 173);
+            this.menos.Name = "menos";
+            this.menos.Size = new System.Drawing.Size(157, 55);
+            this.menos.TabIndex = 4;
+            this.menos.Text = "-";
+            this.menos.UseVisualStyleBackColor = true;
+            this.menos.Click += new System.EventHandler(this.menos_Click);
+            // 
+            // multiplicacao
+            // 
+            this.multiplicacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiplicacao.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.multiplicacao.Location = new System.Drawing.Point(116, 101);
+            this.multiplicacao.Name = "multiplicacao";
+            this.multiplicacao.Size = new System.Drawing.Size(157, 55);
+            this.multiplicacao.TabIndex = 5;
+            this.multiplicacao.Text = "*";
+            this.multiplicacao.UseVisualStyleBackColor = true;
+            this.multiplicacao.Click += new System.EventHandler(this.multiplicacao_Click);
+            // 
+            // divisao
+            // 
+            this.divisao.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divisao.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.divisao.Location = new System.Drawing.Point(116, 173);
+            this.divisao.Name = "divisao";
+            this.divisao.Size = new System.Drawing.Size(157, 55);
+            this.divisao.TabIndex = 6;
+            this.divisao.Text = "/";
+            this.divisao.UseVisualStyleBackColor = true;
+            this.divisao.Click += new System.EventHandler(this.divisao_Click);
+            // 
+            // txtVisor
+            // 
+            this.txtVisor.AutoSize = true;
+            this.txtVisor.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVisor.Location = new System.Drawing.Point(270, 269);
+            this.txtVisor.Name = "txtVisor";
+            this.txtVisor.Size = new System.Drawing.Size(0, 29);
+            this.txtVisor.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(469, 369);
+            this.Controls.Add(this.txtVisor);
+            this.Controls.Add(this.divisao);
+            this.Controls.Add(this.multiplicacao);
+            this.Controls.Add(this.menos);
+            this.Controls.Add(this.soma);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CALCULADORA";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.Button soma;
+        private System.Windows.Forms.Button menos;
+        private System.Windows.Forms.Button multiplicacao;
+        private System.Windows.Forms.Button divisao;
+        private System.Windows.Forms.Label txtVisor;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculo_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void multiplicacao_Click(object sender, EventArgs e)
+        {
+            double n1 = double.Parse(txtN1.Text);
+            double n2 = double.Parse(txtN2.Text);
+            double resultado = n1 * n2;
+            MessageBox.Show($"Resultado: {resultado}");
+            txtVisor.Text = (n1 * n2).ToString();
+        }
+
+        private void soma_Click(object sender, EventArgs e)
+        {
+            double n1 = double.Parse(txtN1.Text);
+            double n2 = double.Parse(txtN2.Text);
+            double resultado = n1 + n2;
+            MessageBox.Show($"Resultado: {resultado}");
+            txtVisor.Text = (n1 + n2).ToString();
+           
+        }
+
+        private void divisao_Click(object sender, EventArgs e)
+        {
+            double n1 = double.Parse(txtN1.Text);
+            double n2 = double.Parse(txtN2.Text);
+            double  resultado =  n1 / n2;
+            MessageBox.Show($"Resultado: {resultado}");
+            if (n2 == 0)
+            {
+                MessageBox.Show("Não é possivel dividir por zero!");
+                return;
+            }
+            txtVisor.Text = (n1 / n2).ToString();
+        }
+
+        private void menos_Click(object sender, EventArgs e)
+        {
+            double n1 = double.Parse(txtN1.Text);
+            double n2 = double.Parse(txtN2.Text);
+            double resultado = n1 - n2;
+            MessageBox.Show($"Resultado: {resultado}");
+
+            txtVisor.Text = (n1 - n2).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
